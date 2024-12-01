b333b8880adfe7d4931c49cfb49e0d8a543784c7
DESKTOP-MMDPG7A\thoma <thomaspstaples@gmail.com>
2024-12-01 11:45:31 +0000

    Minor optimization for part two creating the hashmap when parsing the list initially

--- day_one/main.py
+++ day_one/main.py
@@ -4,7 +4,7 @@ class Solution:
     def __init__(self):
         self._list_one = []
         self._list_two = []
-
+        self._second_list_frequency_hash_map = {}
     
     def read_puzzle_input(self):
         with open('puzzle_input.txt') as f:
@@ -12,8 +12,14 @@ class Solution:
         
         for entry in input:
             _entries = entry.split("   ")
-            self._list_one.append(int(_entries[0]))
-            self._list_two.append(int(_entries[1].split("\n")[0]))
+            _list_one_entry = int(_entries[0])
+            _list_two_entry = int(_entries[1].split("\n")[0])
+            self._list_one.append(_list_one_entry)
+            self._list_two.append(_list_two_entry)
+            if _list_two_entry not in self._second_list_frequency_hash_map:
+                self._second_list_frequency_hash_map[_list_two_entry] = 1
+            else:
+                self._second_list_frequency_hash_map[_list_two_entry] += 1
 
     def part_one(self):
         _part_one_start = perf_counter()
@@ -28,25 +34,13 @@ class Solution:
         print(f"Part one Answer {_total_distance_in_list} Duration {_part_one_end - _part_one_start}")
         
     def part_two(self):
-        # Create hash map of second list 
-        # Containing num : frequency 
         _part_two_start = perf_counter()
-        _scaling_hashmap = {}
         _similarity_score = 0
-        for num in self._list_two:
-            if num not in _scaling_hashmap:
-                _scaling_hashmap[num] = 1
-            else:
-                _scaling_hashmap[num] += 1
-        # Calculate Scaling score
         for num in self._list_one:
-            if num in _scaling_hashmap:
-                _similarity_score += num * _scaling_hashmap[num]
+            if num in self._second_list_frequency_hash_map:
+                _similarity_score += num * self._second_list_frequency_hash_map[num]
         _part_two_end = perf_counter()
         print(f"Part Two Answer {_similarity_score} Duration {_part_two_end - _part_two_start}")
-
-
-
 
 if __name__ == "__main__":
     _solution = Solution()
